00eb8a40f6b30cb5acadc115aec6dc510683aa4b
InaraShalfei <inarashugaikina@gmail.com>
2023-05-11 15:05:40 +0600

    Create project initial models and make migrations

--- luvr_bot/telegram_bot/models.py
+++ luvr_bot/telegram_bot/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    phone_number = models.CharField(max_length=250)
+
+
+class EmployeeGeoPosition(models.Model):
+    employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='geo_positions')
+    latitude = models.CharField(max_length=300)
+    longitude = models.CharField(max_length=300)
+
+
+class BranchGeoPosition(models.Model):
+    branch_name = models.CharField(max_length=250)
+    latitude = models.CharField(max_length=300)
+    longitude = models.CharField(max_length=300)
