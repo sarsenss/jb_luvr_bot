1769cdfdcd5a4e726af5c6a0a250090a27ffa08b
InaraShalfei <inarashugaikina@gmail.com>
2023-05-15 14:27:04 +0600

    Intermediate solution for project main functionality

--- luvr_bot/telegram_bot/urls.py
+++ luvr_bot/telegram_bot/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.say_hi, name='say_hi'),
-    path('start/', views.wake_up, name='start')
+    path('', views.main_func, name='main_fucntion'),
+    path('start/', views.start, name='start')
  ]
--- luvr_bot/telegram_bot/views.py
+++ luvr_bot/telegram_bot/views.py
@@ -5,6 +5,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from dotenv import load_dotenv
 from django.http import HttpResponse
 
+from .models import Employee
 
 load_dotenv()
 
@@ -14,42 +15,48 @@ bot = Bot(token=token)
 updater = Updater(token)
 
 
-def say_hi(update, context):
-    # if chat_id not in employees - send keyboard with request contact button and exit
-    print(update)
+def main_func(update, context):
     chat = update.effective_chat
-    context.bot.send_message(chat_id=chat.id, text='Привет, я Bot!')
+    if not Employee.objects.filter(chat_id=chat.id):
+        phone_button = KeyboardButton(text='Отправить номер телефона', request_contact=True)
+        context.bot.send_message(chat_id=chat.id,
+                                 text=f'Пожалуйста, отправьте мне свой номер телефона для регистрации '
+                                      f'(кнопка "Отправить номер телефона")',
+                                 reply_markup=ReplyKeyboardMarkup([[phone_button]]))
+
+    context.bot.send_message(chat_id=chat.id, text='Спасибо, что поделились номером телефона!\n'
+                                                   'Для начала смены нажмите кнопку "Начать смену"')
+    phone_number = update.message.contact.phone_number
+    employee = Employee.objects.filter(chat_id=chat.id)
+    employee.phone_number = phone_number
+    location_button = KeyboardButton(text='Отправить координаты', request_location=True)
+    context.bot.send_message(chat_id=chat.id,
+                             text=f'Спасибо, мы записали ваши данные о начале смены.\n'
+                                  f'Не забудьте завершить смену, нажав на кнопку "Закончить смену"',
+                             reply_markup=ReplyKeyboardMarkup([[location_button]]))
+
     return HttpResponse()
 
 
-def wake_up(update, context):
+def start(update, context):
     chat = update.effective_chat
-    print(update)
+    if not Employee.objects.filter(chat_id=chat.id):
+        Employee.objects.create(chat_id=chat.id)
     name = update.message.chat.first_name
-    print(update.message)
-    phone_button = KeyboardButton(text='Присоединиться', request_contact=True)
-    start_shift_button = KeyboardButton(text='Начать смену')
-    end_shift_button = KeyboardButton(text='Закончить смену')
-    send_location_button = KeyboardButton(text='Отправить координаты', request_location=True)
+    phone_button = KeyboardButton(text='Отправить номер телефона', request_contact=True)
+    # start_shift_button = KeyboardButton(text='Начать смену')
+    # end_shift_button = KeyboardButton(text='Закончить смену')
+    # send_location_button = KeyboardButton(text='Отправить координаты', request_location=True)
     context.bot.send_message(chat_id=chat.id,
-                             text=f'Спасибо, что включили меня, {name}', reply_markup=ReplyKeyboardMarkup(
-            [
-                [phone_button],
-                [start_shift_button, end_shift_button],
-                [send_location_button]
-            ]
-        )
-                             )
+                             text=f'Спасибо, что включили меня, {name}!\n'
+                                  f'Пожалуйста, отправьте мне свой номер телефона для регистрации '
+                                  f'(кнопка "Отправить номер телефона")', reply_markup=ReplyKeyboardMarkup([[phone_button]]))
 
 
-
-
-
-updater.dispatcher.add_handler(CommandHandler('start', wake_up))
-
-updater.dispatcher.add_handler(MessageHandler(Filters.text, say_hi))
-updater.dispatcher.add_handler(MessageHandler(Filters.contact, say_hi))
-updater.dispatcher.add_handler(MessageHandler(Filters.location, say_hi))
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, main_func))
+updater.dispatcher.add_handler(MessageHandler(Filters.contact, main_func))
+updater.dispatcher.add_handler(MessageHandler(Filters.location, main_func))
 updater.start_polling()
 # updater.idle()
 
