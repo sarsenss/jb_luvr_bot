92e9b849077ed95bac02a0b6b8233769dd47c88a
InaraShalfei <inarashugaikina@gmail.com>
2023-05-15 11:10:05 +0600

    Add __str__ methods, verbose names to models and reqgister them in admin panel

--- luvr_bot/telegram_bot/admin.py
+++ luvr_bot/telegram_bot/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Employee, EmployeeGeoPosition, Branch, JobRequest, JobRequestAssignment
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('phone_number',)
+
+
+class EmployeeGeoPositionAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'latitude', 'longitude',)
+
+
+class BranchAdmin(admin.ModelAdmin):
+    list_display = ('branch_name', 'latitude', 'longitude',)
+
+
+class JobRequestAdmin(admin.ModelAdmin):
+    list_display = ('branch', 'employee_position', 'request_type', 'date_start', 'date_end', 'shift_date', 'shift_hours',
+                    'number_of_employees', 'request_comment',)
+
+
+class JobRequestAssignmentAdmin(admin.ModelAdmin):
+    list_display = ('job_request', 'employee',)
+
+
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(EmployeeGeoPosition, EmployeeGeoPositionAdmin)
+admin.site.register(Branch, BranchAdmin)
+admin.site.register(JobRequest, JobRequestAdmin)
+admin.site.register(JobRequestAssignment, JobRequestAssignmentAdmin)
--- luvr_bot/telegram_bot/models.py
+++ luvr_bot/telegram_bot/models.py
@@ -2,16 +2,63 @@ from django.db import models
 
 
 class Employee(models.Model):
-    phone_number = models.CharField(max_length=250)
+    phone_number = models.CharField(max_length=250, verbose_name='номер телефона')
+
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
+
+    def __str__(self):
+        return self.phone_number
 
 
 class EmployeeGeoPosition(models.Model):
-    employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='geo_positions')
-    latitude = models.CharField(max_length=300)
-    longitude = models.CharField(max_length=300)
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='geo_positions', verbose_name='сотрудник')
+    latitude = models.CharField(max_length=300, verbose_name='широта')
+    longitude = models.CharField(max_length=300, verbose_name='долгота')
+
+    class Meta:
+        verbose_name = 'Геопозиция сотрудника'
+        verbose_name_plural = 'Геопозиция сотрудников'
 
 
-class BranchGeoPosition(models.Model):
-    branch_name = models.CharField(max_length=250)
-    latitude = models.CharField(max_length=300)
-    longitude = models.CharField(max_length=300)
+class Branch(models.Model):
+    branch_name = models.CharField(max_length=250,  verbose_name='название филиала')
+    latitude = models.CharField(max_length=300,  verbose_name='широта')
+    longitude = models.CharField(max_length=300,  verbose_name='долгота')
+
+    class Meta:
+        verbose_name = 'Филиал'
+        verbose_name_plural = 'Филиалы'
+
+    def __str__(self):
+        return self.branch_name
+
+
+class JobRequest(models.Model):
+    # deal_id
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, related_name='job_requests')
+    employee_position = models.CharField(blank=True, null=True, max_length=300, verbose_name='должность')
+    request_type = models.CharField(blank=True, null=True, max_length=250, verbose_name='тип заявки')
+    date_start = models.DateField(blank=True, null=True, verbose_name='дата начала периода')
+    date_end = models.DateField(blank=True, null=True, verbose_name='дата окончания периода')
+    shift_date = models.DateField(blank=True, null=True, verbose_name='дата смены')
+    shift_hours = models.DateTimeField(blank=True, null=True, verbose_name='время смены')
+    number_of_employees = models.CharField(max_length=3, blank=True, null=True, verbose_name='количество сотрудников')
+    request_comment = models.TextField(blank=True, null=True, verbose_name='комментарий')
+
+    class Meta:
+        verbose_name = 'Заявка на сотрудников'
+        verbose_name_plural = 'Заявки на сотрудников'
+
+    def __str__(self):
+        return f'{self.request_type} - {self.branch}'
+
+
+class JobRequestAssignment(models.Model):
+    job_request = models.ForeignKey(JobRequest, on_delete=models.CASCADE, related_name='assignments', verbose_name='заявка на сотрудника')
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='assignments', verbose_name='сотрудник')
+
+    class Meta:
+        verbose_name = 'Назначение сотрудников'
+        verbose_name_plural = 'Назначения сотрудников'
