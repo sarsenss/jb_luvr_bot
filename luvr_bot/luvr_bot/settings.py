be6dc2140b879b2635348fe85c354f4f91588a7e
InaraShalfei <inarashugaikina@gmail.com>
2023-05-30 14:59:38 +0600

    Register custom User model

--- luvr_bot/luvr_bot/settings.py
+++ luvr_bot/luvr_bot/settings.py
@@ -110,3 +110,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'telegram_bot.CustomUser'
